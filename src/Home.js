cf208e3c8a24894b1a843532e972e2d695c6ee3a
Sunil Singampalli <sunilsingampallis@gmail.com>
2019-06-15 10:22:23 +0530

    home page

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+    Container,
+    Carousel,
+    Row,
+    Col
+}
+from 'react-bootstrap';
 import Header from './Header';
 
 
@@ -14,7 +21,35 @@ class Home extends Component {
         return(
             <div>
                 <Header />
-                
+            {/* <Container>
+                <Row>
+                    <Col lg={12} md={12} sm={12} xs={12}>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="./img/sliderimage1.jpg"
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="./img/sliderimage2.jpg"
+                        alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    </Carousel>
+                    </Col>
+                    </Row>
+                </Container> */}
 
             </div>
         );
